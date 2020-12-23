003a8809d8da4ac3395db87a99dd2271aed09b9f
lsyer <lishao378@sohu.com>
2020-12-23 11:47:08 +0800

    1.将master与proxy分离为两个docker；2.添加proxy中v2ray自动安装；3.添加镜像编译和使用文档。

---
+++ init_proxyonly.sh
@@ -0,0 +1,6 @@
+cp /opt/conf/v2ray-mng.conf /etc/nginx/sites-enabled/default
+nohup java -jar -Xms40m -Xmx40m -XX:MaxDirectMemorySize=100M -XX:MaxMetaspaceSize=180m /opt/jar/v2ray-proxy.jar --spring.config.location=/opt/conf/proxy.yaml >> /opt/proxy.log &
+mv /etc/v2ray/config.json /etc/v2ray/config.json.bak
+cp /opt/conf/config.json /etc/v2ray/config.json
+nohup /usr/bin/v2ray -config=/etc/v2ray/config.json &
+nginx -g 'daemon off;'
