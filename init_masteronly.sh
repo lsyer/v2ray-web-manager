1b774f3f5734cec74e9cbc84121db6cd2dd4e75a
lsyer <lishao378@sohu.com>
2020-12-23 11:54:21 +0800

    修复master与proxy的启动脚本

--- init_masteronly.sh
+++ init_masteronly.sh
@@ -1,3 +1,3 @@
-cp /opt/conf/v2ray-mng.conf /etc/nginx/sites-enabled/default
+cp /opt/conf/v2ray-mng_masteronly.conf /etc/nginx/sites-enabled/default
 nohup java -jar -Xms40m -Xmx40m -XX:MaxDirectMemorySize=100M -XX:MaxMetaspaceSize=180m /opt/jar/admin.jar --spring.config.location=/opt/conf/admin.yaml >> /opt/admin.log &
 nginx -g 'daemon off;'
--- init_proxyonly.sh
+++ init_proxyonly.sh
@@ -1,4 +1,4 @@
-cp /opt/conf/v2ray-mng.conf /etc/nginx/sites-enabled/default
+cp /opt/conf/v2ray-mng_proxyonly.conf /etc/nginx/sites-enabled/default
 nohup java -jar -Xms40m -Xmx40m -XX:MaxDirectMemorySize=100M -XX:MaxMetaspaceSize=180m /opt/jar/v2ray-proxy.jar --spring.config.location=/opt/conf/proxy.yaml >> /opt/proxy.log &
 mv /etc/v2ray/config.json /etc/v2ray/config.json.bak
 cp /opt/conf/config.json /etc/v2ray/config.json
